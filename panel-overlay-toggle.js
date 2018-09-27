638c9937006cfae85873c65794fcf8960c470150
luetage <luetage@zoho.com>
2018-09-27 15:15:49 +0200

    panel overlay improvement

--- panel-overlay-toggle.js
+++ panel-overlay-toggle.js
@@ -4,34 +4,18 @@ https://forum.vivaldi.net/topic/10590/overlay-panels/151
 A custom button to toggle the panel overlay mode right from the panels container A previous version toggled css code to enable an independent overlay and can be found here: https://forum.vivaldi.net/topic/10590/overlay-panels/36
 */
 
-function overlayToggle() {
+unction overlayToggle() {
 
-    // create button
-    const circE = 'M 13 13m -6, 0a 6,6 0 1,0 12,0a 6,6 0 1,0 -12,0 M 13 13m -4, 0a 4,4 0 1,0 8,0a 4,4 0 1,0 -8,0 M 13 13m -2, 0a 2,2 0 1,0 4,0a 2,2 0 1,0 -4,0';
-    const circD = 'M 13 13m -5.5, 0a 5.5,5.5 0 1,0 11,0a 5.5,5.5 0 1,0 -11,0 M 13 13m -2, 0a 2,2 0 1,0 4,0a 2,2 0 1,0 -4,0';
-    const switchS = document.getElementById('switch');
-    var btnS = document.createElement('button');
-    btnS.classList.add('preferences');
-    btnS.id = 'overlayToggle';
-    btnS.title = 'Toggle Overlay';
-    btnS.setAttribute('tabindex', '-1');
-    btnS.innerHTML = '<svg width="26" height="26" viewBox="0 0 26 26" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd"></path></svg>';
-    switchS.lastChild.style = 'margin-top: 0px';
-    switchS.insertBefore(btnS,switchS.lastChild);
+    function icn() {
+        if (cont.classList.contains('overlay') && exec === 0 || !cont.classList.contains('overlay') && exec === 1) {
+            pathS.setAttribute('d', circE);
+        }
+        else {
+            pathS.setAttribute('d', circD);
+        }
+        exec = 1
+    };
 
-    // startup setting
-    const cont = document.getElementById('panels-container');
-    const pathS = document.querySelector('#overlayToggle svg path');
-    if (cont.classList.contains('overlay')) {
-        pathS.setAttribute('d', circE);
-        var mode = 1;
-    }
-    else {
-        pathS.setAttribute('d', circD);
-        var mode = 0;
-    }
-
-    // toggle logic
     function simulateClick() {
         const toggle = document.querySelector('.paneltogglefooter');
         var evt = new MouseEvent("click", {
@@ -41,15 +25,24 @@ function overlayToggle() {
             altKey: true
         });
         toggle.dispatchEvent(evt);
-        if (mode === 0) {
-            pathS.setAttribute('d', circE);
-            mode = 1;
-        }
-        else {
-            pathS.setAttribute('d', circD);
-            mode = 0;
-        }
+        icn();
     };
+
+    var exec = 0;
+    const circE = 'M 13 13m -6, 0a 6,6 0 1,0 12,0a 6,6 0 1,0 -12,0 M 13 13m -4, 0a 4,4 0 1,0 8,0a 4,4 0 1,0 -8,0 M 13 13m -2, 0a 2,2 0 1,0 4,0a 2,2 0 1,0 -4,0';
+    const circD = 'M 13 13m -5.5, 0a 5.5,5.5 0 1,0 11,0a 5.5,5.5 0 1,0 -11,0 M 13 13m -2, 0a 2,2 0 1,0 4,0a 2,2 0 1,0 -4,0';
+    const cont = document.getElementById('panels-container');
+    const switchS = document.getElementById('switch');
+    var btnS = document.createElement('button');
+    btnS.classList.add('preferences');
+    btnS.id = 'overlayToggle';
+    btnS.title = 'Toggle Overlay';
+    btnS.setAttribute('tabindex', '-1');
+    btnS.innerHTML = '<svg width="26" height="26" viewBox="0 0 26 26" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd"></path></svg>';
+    switchS.lastChild.style = 'margin-top: 0px';
+    switchS.insertBefore(btnS,switchS.lastChild);
+    const pathS = document.querySelector('#overlayToggle svg path');
+    icn();
     document.getElementById('overlayToggle').addEventListener('click', simulateClick);
 };
 
