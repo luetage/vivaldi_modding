fceb383b888a89d004f821eb88d45fa3c5329b55
luetage <luetage@zoho.com>
2018-10-06 22:22:12 +0200

    link added

--- tab_lock.js
+++ tab_lock.js
@@ -1,6 +1,6 @@
 /*
 Tab Lock
-https://forum.vivaldi.net/topic/14723/legacy-panel
+https://forum.vivaldi.net/topic/30957/tab-lock
 Download this file and add it to Vivaldi's user_files folder within the application. This will make the mod available as page action in the status bar.
 */
 
