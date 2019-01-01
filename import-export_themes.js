02bfabc153b778f4bcd5774665ad09eb35d63f6a
luetage <luetage@zoho.com>
2019-01-01 22:39:44 +0100

    import/export theme code instead of files

--- import-export_themes.js
+++ import-export_themes.js
@@ -1,81 +1,69 @@
 /*
 Theme Import and Export
 https://forum.vivaldi.net/topic/33154/import-and-export-themes
-Adds Import and Export button to Vivaldi's theme page when clicking the +/add or pencil/edit button. Saves themes in .json format. Enables backing up and sharing themes.
+Adds Import and Export button to Vivaldi's theme page when clicking the +/add or pencil/edit button. Saves themes in .json format (version 1) or exports theme code directly (version 2). Enables backing up and sharing themes. This here is version 2. Version 1 is available through the link above.
 */
 
-function _importTheme(e) {
-    var files = e.target.files, reader = new FileReader();
-    reader.onload = _imp;
-    reader.readAsText(files[0]);
-};
-function _imp() {
-    var colors = JSON.parse(this.result);
-    chrome.storage.local.set(
-        colors,
-    function() {
-        chrome.storage.local.get({
-            'themeName': '',
-            'themeBg': '',
-            'themeFg': '',
-            'themeHi': '',
-            'themeAc': '',
-            'themePage': '',
-            'themeWin': '',
-            'themeTabs': '',
-            'themeRound': ''
-        },
-        function(set) {
-            _themeName.select();
-            document.execCommand('insertText', false, set.themeName);
-            setTimeout(function() {
-                _themeBg.select();
-                document.execCommand('insertText', false, set.themeBg);
-            }, 200);
-            setTimeout(function() {
-                _themeFg.select();
-                document.execCommand('insertText', false, set.themeFg);
-            }, 400);
-            setTimeout(function() {
-                _themeHi.select();
-                document.execCommand('insertText', false, set.themeHi);
-            }, 600);
-            setTimeout(function() {
-                _themeAc.select();
-                document.execCommand('insertText', false, set.themeAc);
-            }, 800);
-            if ((set.themePage === 1 && !_themePage.checked) || (set.themePage === 0 && _themePage.checked)) {
-                setTimeout(function() {
-                    _themePage.click();
-                }, 1000);
-            }
-            if ((set.themeWin === 1 && !_themeWin.checked) || (set.themeWin === 0 && _themeWin.checked)) {
-                setTimeout(function() {
-                    _themeWin.click();
-                }, 1200);
-            }
-            if ((set.themeTabs === 1 && !_themeTabs.checked) || (set.themeTabs === 0 && _themeTabs.checked)) {
-                setTimeout(function() {
-                    _themeTabs.click();
-                }, 1400);
-            }
-            setTimeout(function () {
-                const disp = document.querySelector('.border-radius label span');
-                if (set.themeRound === '-1') {
-                    disp.innerText = 'Disabled';
-                }
-                else if (set.themeRound === '0') {
-                    disp.innerText = 'Default';
-                }
-                else {
-                    disp.innerText = set.themeRound + 'px';
-                }
-                _themeRound.value = set.themeRound;
-                _themeRound.focus();
-            }, 1600);
-        });
-    });
-    document.getElementById('importHidden').value = '';
+/* Theme Import and Export */
+
+function _importTheme() {
+    event.stopPropagation();
+    event.preventDefault();
+    if (_eventType === 'paste') {
+        var clipboardData = event.clipboardData || window.clipboardData;
+        var themeCode = clipboardData.getData('text');
+    }
+    else {
+        var themeCode = event.dataTransfer.getData('text');
+    }
+    var set = JSON.parse(themeCode);
+    _themeName.select();
+    document.execCommand('insertText', false, set.themeName);
+    setTimeout(function() {
+        _themeBg.select();
+        document.execCommand('insertText', false, set.themeBg);
+    }, 200);
+    setTimeout(function() {
+        _themeFg.select();
+        document.execCommand('insertText', false, set.themeFg);
+    }, 400);
+    setTimeout(function() {
+        _themeHi.select();
+        document.execCommand('insertText', false, set.themeHi);
+    }, 600);
+    setTimeout(function() {
+        _themeAc.select();
+        document.execCommand('insertText', false, set.themeAc);
+    }, 800);
+    if ((set.themePage === 1 && !_themePage.checked) || (set.themePage === 0 && _themePage.checked)) {
+        setTimeout(function() {
+            _themePage.click();
+        }, 1000);
+    }
+    if ((set.themeWin === 1 && !_themeWin.checked) || (set.themeWin === 0 && _themeWin.checked)) {
+        setTimeout(function() {
+            _themeWin.click();
+        }, 1200);
+    }
+    if ((set.themeTabs === 1 && !_themeTabs.checked) || (set.themeTabs === 0 && _themeTabs.checked)) {
+        setTimeout(function() {
+            _themeTabs.click();
+        }, 1400);
+    }
+    setTimeout(function () {
+        const disp = document.querySelector('.border-radius label span');
+        if (set.themeRound === '-1') {
+            disp.innerText = 'Disabled';
+        }
+        else if (set.themeRound === '0') {
+            disp.innerText = 'Default';
+        }
+        else {
+            disp.innerText = set.themeRound + 'px';
+        }
+        _themeRound.value = set.themeRound;
+        _themeRound.focus();
+    }, 1600);
 };
 
 function _exportTheme() {
@@ -94,63 +82,26 @@ function _exportTheme() {
     else {
         var checkTabs = 0;
     }
-    chrome.storage.local.set({
-        'themeName': _themeName.value,
-        'themeBg': _themeBg.value,
-        'themeFg': _themeFg.value,
-        'themeHi': _themeHi.value,
-        'themeAc': _themeAc.value,
-        'themePage': checkPage,
-        'themeWin': checkWin,
-        'themeTabs': checkTabs,
-        'themeRound': _themeRound.value
-    },
-    function() {
-        chrome.storage.local.get({
-            'themeName': '',
-            'themeBg': '',
-            'themeFg': '',
-            'themeHi': '',
-            'themeAc': '',
-            'themePage': '',
-            'themeWin': '',
-            'themeTabs': '',
-            'themeRound': ''
-        },
-        function(items){
-            var result = JSON.stringify(items);
-            var url = 'data:application/json;base64,' + btoa(result);
-            const getName = items.themeName.replace(/\s+/g, '-').toLowerCase();
-            if (getName !== '' && tryAgain === false) {
-                var nameIt = getName + '.json';
-            }
-            else {
-                var nameIt = 'theme.json';
-            }
-            chrome.downloads.download({
-                url: url,
-                saveAs: true,
-                filename: nameIt
-            },
-            function(download) {
-                if (download === undefined && tryAgain === false) {
-                    console.log('export error: ' + chrome.runtime.lastError.message);
-                    tryAgain = true;
-                    _exportTheme();
-                }
-                else {
-                    tryAgain = false;
-                }
-            });
-        });
-    });
+    const share = {'themeName': _themeName.value, 'themeBg': _themeBg.value, 'themeFg': _themeFg.value, 'themeHi': _themeHi.value, 'themeAc': _themeAc.value, 'themePage': checkPage, 'themeWin': checkWin, 'themeTabs': checkTabs, 'themeRound': _themeRound.value};
+    const themeCode = JSON.stringify(share);
+    navigator.clipboard.writeText(themeCode);
+    const confirm = document.createElement('span');
+    confirm.innerText = 'Theme code copied to clipboard.';
+    confirm.style = 'color: var(--colorHighlightBg); margin-left: 6px; margin-top: 6px;)';
+    confirm.id = 'confirmExport';
+    const confirmExport = document.getElementById('confirmExport');
+    if (!confirmExport) {
+        _cont.appendChild(confirm);
+        setTimeout( function() {
+        _cont.removeChild(confirm);
+        }, 3500);
+    }
 };
 
 function _buttons() {
     const check = document.getElementById('importTheme');
-    const cont = document.querySelector('.theme-metadata');
-    if (cont && !check) {
-        tryAgain = false;
+    _cont = document.querySelector('.theme-metadata');
+    if (_cont && !check) {
         _themeName = document.querySelector('.theme-name');
         _themeBg = document.querySelector('.theme-colors div:nth-of-type(1) input');
         _themeFg = document.querySelector('.theme-colors div:nth-of-type(2) input');
@@ -161,30 +112,32 @@ function _buttons() {
         _themeTabs = document.querySelector('.theme-settings div div:nth-of-type(3) label input');
         _themeRound = document.querySelector('.border-radius label input');
         var importBtn = document.createElement('input');
-        importBtn.setAttribute('type', 'submit');
-        importBtn.classList.add('primary');
-        importBtn.setAttribute('value', 'Import');
-        importBtn.style.marginLeft = '6px';
+        importBtn.setAttribute('type', 'text');
+        importBtn.setAttribute('placeholder', 'Import');
+        importBtn.style = 'margin-left: 6px;  ';
         importBtn.id = 'importTheme';
-        cont.appendChild(importBtn);
+        _cont.appendChild(importBtn);
+        const style = document.createElement('style');
+        style.type = 'text/css';
+        style.innerHTML = '#importTheme {width: 80px;} #importTheme::-webkit-input-placeholder {opacity: 1; color: var(--colorHighlightBg); text-align: center;}';
+        document.getElementsByTagName('head')[0].appendChild(style);
         var exportBtn = document.createElement('input');
         exportBtn.setAttribute('type', 'submit');
         exportBtn.classList.add('primary');
         exportBtn.setAttribute('value', 'Export');
         exportBtn.style.marginLeft = '6px';
         exportBtn.id = 'exportTheme';
-        cont.appendChild(exportBtn);
-        var  hiddenBtn = document.createElement('input');
-        hiddenBtn.setAttribute('type','file');
-        hiddenBtn.id = 'importHidden';
-        hiddenBtn.setAttribute('accept','.json');
-        hiddenBtn.style.display = 'none';
-        cont.appendChild(hiddenBtn);
-        importBtn.onclick = function() {
-            document.getElementById('importHidden').click();
-        };
-        document.getElementById('importHidden').addEventListener("change", _importTheme, false);
+        _cont.appendChild(exportBtn);
         document.getElementById('exportTheme').addEventListener('click', _exportTheme);
+        const importInput = document.getElementById('importTheme');
+        importInput.addEventListener('paste', function() {
+            _eventType = 'paste';
+            _importTheme(event);
+        });
+        importInput.addEventListener('drop', function() {
+            _eventType = 'drop';
+            _importTheme(event);
+        });
     }
 };
 
