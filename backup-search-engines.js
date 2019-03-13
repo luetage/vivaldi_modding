d2205c269400372d4a69424fab8ec3ddcdce5a69
luetage <luetage@zoho.com>
2019-03-13 16:21:18 +0100

    new mod: backup search engines

---
+++ backup-search-engines.js
@@ -0,0 +1,108 @@
+/*
+Backup Search Engines
+https://forum.vivaldi.net/topic/33154/import-and-export-themes
+Adds functionality to backup and restore search engines in vivaldi://settings/search.
+*/
+
+function _msgSearch(pnt) {
+    clearTimeout(_msgTimeout);
+    if (pnt === 'backup') {
+        _infoSearch.innerText = 'Search engines backup copied to clipboard.';
+    }
+    else if (pnt === 'restore') {
+        _infoSearch.innerText = 'Search engines restored.';
+    }
+    else {
+        _infoSearch.innerText = 'Search engines code error.'
+    }
+    _msgTimeout = setTimeout(function() {
+        _infoSearch.innerText = '';
+    }, 50000);
+};
+
+
+function _restoreSearch() {
+    event.preventDefault();
+    event.stopPropagation();
+    if (_eventSearch === 'paste') {
+        var clipboardData = event.clipboardData || window.clipboardData;
+        var engineCode = clipboardData.getData('text');
+    }
+    else {
+        var engineCode = event.dataTransfer.getData('text');
+    }
+    try {
+        engines = JSON.parse(engineCode);
+    }
+    catch(err) {
+        _msgSearch('error');
+        return;
+    }
+    if (Object.keys(engines)[0] === 'context') {
+        chrome.storage.local.set({'SEARCH_ENGINE_COLLECTION': engines}, function() {
+            _msgSearch('restore');
+        });
+    }
+    else {
+        _msgSearch('error');
+    }
+};
+
+function _backupSearch() {
+    chrome.storage.local.get({'SEARCH_ENGINE_COLLECTION': ''}, function(back) {
+        const engines = back.SEARCH_ENGINE_COLLECTION;
+        const engineCode = JSON.stringify(engines);
+        navigator.clipboard.writeText(engineCode);
+        _msgSearch('backup');
+    });
+};
+
+function searchEngines() {
+    const search = document.querySelector('.setting-search-engine');
+    const check = document.getElementById('backupSearch');
+    if (search && !check) {
+        const place = document.querySelector('.setting-section .setting-group.unlimited .setting-single');
+        const style = document.createElement('style');
+        style.type = 'text/css';
+        style.innerHTML = '#backupSearch, #restoreSearch {margin-left: var(--padding);}#restoreSearch{width: 130px;margin-top: var(--padding);}#restoreSearch::-webkit-input-placeholder {opacity: 1;color: var(--colorHighlightBg);text-align: center;}';
+        document.getElementsByTagName('head')[0].appendChild(style);
+        const backupBtn = document.createElement('input');
+        backupBtn.setAttribute('type', 'button');
+        backupBtn.setAttribute('value', 'Backup');
+        backupBtn.id = 'backupSearch';
+        place.insertBefore(backupBtn, place.lastChild);
+        const restoreInput = document.createElement('input');
+        restoreInput.setAttribute('type', 'text');
+        restoreInput.setAttribute('placeholder', 'Restore Backup');
+        restoreInput.id = 'restoreSearch';
+        place.insertBefore(restoreInput, place.lastChild);
+        _infoSearch = document.createElement('span');
+        _infoSearch.id = 'msgConfirm';
+        place.insertBefore(_infoSearch, place.lastChild);
+        document.getElementById('backupSearch').addEventListener('click', _backupSearch);
+        const restoreSearch = document.getElementById('restoreSearch');
+        restoreSearch.addEventListener('paste', function() {
+            _eventSearch = 'paste';
+            _restoreSearch(event);
+        });
+        restoreSearch.addEventListener('drop', function() {
+            _eventSearch = 'drop';
+            _restoreSearch(event);
+        });
+        _msgTimeout = {};
+    }
+};
+
+// Loop waiting for the browser to load the UI. You can call all functions from just one instance.
+
+setTimeout(function wait() {
+    const browser = document.getElementById('browser');
+    if (browser) {
+        document.body.addEventListener('click', function() {
+            setTimeout(searchEngines, 50);
+        });
+    }
+    else {
+        setTimeout(wait, 300);
+    }
+}, 300);
