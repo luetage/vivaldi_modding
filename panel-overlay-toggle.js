05773352d08b1ff9e94cee683c2e105b70f0874d
luetage <luetage@zoho.com>
2019-03-28 16:58:45 +0100

    overlay toggle update for 2.4

--- panel-overlay-toggle.js
+++ panel-overlay-toggle.js
@@ -1,7 +1,7 @@
 /*
 Panel Overlay Toggle
 https://forum.vivaldi.net/topic/10590/overlay-panels/151
-A custom button to toggle the panel overlay mode right from the panels container A previous version toggled css code to enable an independent overlay and can be found here: https://forum.vivaldi.net/topic/10590/overlay-panels/36
+A custom button to toggle the panel overlay mode right from the panels container. The panel toggle must be visible. Move it to the address bar where it will be made invisible by css, then you can reset the status bar to get the panel toggle back and hide or show the status bar as you please.
 */
 
 function overlayToggle() {
@@ -17,18 +17,29 @@ function overlayToggle() {
     };
 
     function simulateClick() {
-        const toggle = document.querySelector('.paneltogglefooter');
-        var evt = new MouseEvent('click', {
+        const toggle = document.querySelector('.panel-clickoutside-ignore button');
+        var down = new PointerEvent('pointerdown', {
             bubbles: true,
             cancelable: true,
-            view: window,
+            pointerType: "mouse",
+            altKey: true
+        });
+        var up = new PointerEvent('pointerup', {
+            bubbles: true,
+            cancelable: true,
+            pointerType: "mouse",
             altKey: true
         });
         icn();
-        toggle.dispatchEvent(evt);
+        toggle.dispatchEvent(down);
+        toggle.dispatchEvent(up);
     };
 
     var exec = 0;
+    const style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = '.toolbar-addressbar .panel-clickoutside-ignore.button-toolbar {width: 0px !important;visibility: hidden;}';
+    document.getElementsByTagName('head')[0].appendChild(style);
     const circE = 'M 13 13m -6, 0a 6,6 0 1,0 12,0a 6,6 0 1,0 -12,0 M 13 13m -4, 0a 4,4 0 1,0 8,0a 4,4 0 1,0 -8,0 M 13 13m -2, 0a 2,2 0 1,0 4,0a 2,2 0 1,0 -4,0';
     const circD = 'M 13 13m -5.5, 0a 5.5,5.5 0 1,0 11,0a 5.5,5.5 0 1,0 -11,0 M 13 13m -2, 0a 2,2 0 1,0 4,0a 2,2 0 1,0 -4,0';
     const cont = document.getElementById('panels-container');
