232a9c55dc1017dbb561a096dada53afd7ea9e59
luetage <luetage@zoho.com>
2019-04-10 20:56:01 +0200

    make statusmod run only one time

--- status-bar-dropdown.js
+++ status-bar-dropdown.js
@@ -88,30 +88,27 @@ function statusStyle() {
 };
 
 function statusMod() {
-    const statusInfoToggle = document.getElementById('statusInfoToggle');
-    if (!statusInfoToggle) {
-        const statusBar = document.querySelector('.toolbar-statusbar');
-        const statusInfo = document.querySelector('.status-info');
-        if (browser.classList.contains('biscuit-mode')) {
-            const version = document.querySelector('.biscuit-string').value
-            const divB = document.createElement('div');
-            divB.classList.add('button-toolbar');
-            divB.id = 'biscuitButton';
-            divB.setAttribute('title', version + '\nClick to copy version string');
-            divB.innerHTML = '<button draggable="false" tabindex="-1"><svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M 12 24 C 17.26 24 20.18 24 22.09 22.09 C 24 20.18 24 17.29 24 12 C 24 6.71 24 3.82 22.09 1.91 C 20.18 0 17.26 0 12 0 C 6.74 0 3.82 0 1.9 1.94 C -0.02 3.88 0 6.77 0 12 C 0 17.23 0 20.21 1.9 22.12 C 3.8 24.03 6.74 24 12 24 Z  M 18.9 8.6 Q 16.11 13.42 13.34 18.24 C 13.042 18.82 12.461 19.199 11.81 19.24 C 11.088 19.323 10.388 18.954 10.05 18.31 Q 8.3 15.31 6.56 12.31 L 4.46 8.58 C 4.12 8.017 4.103 7.315 4.415 6.736 C 4.727 6.157 5.322 5.786 5.98 5.76 C 6.704 5.709 7.386 6.105 7.7 6.76 L 9.25 9.4 C 9.63 10.05 9.99 10.7 10.37 11.34 C 10.868 12.24 11.802 12.813 12.83 12.85 C 14.416 12.94 15.797 11.778 15.98 10.2 C 15.988 10.097 15.988 9.993 15.98 9.89 C 15.981 9.408 15.871 8.933 15.66 8.5 C 15.298 7.843 15.364 7.033 15.827 6.443 C 16.29 5.853 17.061 5.597 17.785 5.792 C 18.509 5.988 19.047 6.597 19.15 7.34 C 19.207 7.776 19.12 8.219 18.9 8.6 Z "></path></svg></button>';
-            statusBar.insertBefore(divB, statusInfo);
-            document.getElementById('biscuitButton').addEventListener('click', function() {
-                navigator.clipboard.writeText(version);
-            });
-        }
-        const divL = document.createElement('divL');
-        divL.classList.add('button-toolbar');
-        divL.id = 'statusInfoToggle';
-        divL.setAttribute('title', 'Toggle status info');
-        divL.innerHTML = '<button draggable="false" tabindex="-1"><svg width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1520 1216q0-40-28-68l-208-208q-28-28-68-28-42 0-72 32 3 3 19 18.5t21.5 21.5 15 19 13 25.5 3.5 27.5q0 40-28 68t-68 28q-15 0-27.5-3.5t-25.5-13-19-15-21.5-21.5-18.5-19q-33 31-33 73 0 40 28 68l206 207q27 27 68 27 40 0 68-26l147-146q28-28 28-67zm-703-705q0-40-28-68l-206-207q-28-28-68-28-39 0-68 27l-147 146q-28 28-28 67 0 40 28 68l208 208q27 27 68 27 42 0 72-31-3-3-19-18.5t-21.5-21.5-15-19-13-25.5-3.5-27.5q0-40 28-68t68-28q15 0 27.5 3.5t25.5 13 19 15 21.5 21.5 18.5 19q33-31 33-73zm895 705q0 120-85 203l-147 146q-83 83-203 83-121 0-204-85l-206-207q-83-83-83-203 0-123 88-209l-88-88q-86 88-208 88-120 0-204-84l-208-208q-84-84-84-204t85-203l147-146q83-83 203-83 121 0 204 85l206 207q83 83 83 203 0 123-88 209l88 88q86-88 208-88 120 0 204 84l208 208q84 84 84 204z"/></svg></button>';
-        statusBar.insertBefore(divL, statusInfo);
-        document.getElementById('statusInfoToggle').addEventListener('click', statusInfoLogic);
+    const statusBar = document.querySelector('.toolbar-statusbar');
+    const statusInfo = document.querySelector('.status-info');
+    if (browser.classList.contains('biscuit-mode')) {
+        const version = document.querySelector('.biscuit-string').value
+        const divB = document.createElement('div');
+        divB.classList.add('button-toolbar');
+        divB.id = 'biscuitButton';
+        divB.setAttribute('title', version + '\nClick to copy version string');
+        divB.innerHTML = '<button draggable="false" tabindex="-1"><svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M 12 24 C 17.26 24 20.18 24 22.09 22.09 C 24 20.18 24 17.29 24 12 C 24 6.71 24 3.82 22.09 1.91 C 20.18 0 17.26 0 12 0 C 6.74 0 3.82 0 1.9 1.94 C -0.02 3.88 0 6.77 0 12 C 0 17.23 0 20.21 1.9 22.12 C 3.8 24.03 6.74 24 12 24 Z  M 18.9 8.6 Q 16.11 13.42 13.34 18.24 C 13.042 18.82 12.461 19.199 11.81 19.24 C 11.088 19.323 10.388 18.954 10.05 18.31 Q 8.3 15.31 6.56 12.31 L 4.46 8.58 C 4.12 8.017 4.103 7.315 4.415 6.736 C 4.727 6.157 5.322 5.786 5.98 5.76 C 6.704 5.709 7.386 6.105 7.7 6.76 L 9.25 9.4 C 9.63 10.05 9.99 10.7 10.37 11.34 C 10.868 12.24 11.802 12.813 12.83 12.85 C 14.416 12.94 15.797 11.778 15.98 10.2 C 15.988 10.097 15.988 9.993 15.98 9.89 C 15.981 9.408 15.871 8.933 15.66 8.5 C 15.298 7.843 15.364 7.033 15.827 6.443 C 16.29 5.853 17.061 5.597 17.785 5.792 C 18.509 5.988 19.047 6.597 19.15 7.34 C 19.207 7.776 19.12 8.219 18.9 8.6 Z "></path></svg></button>';
+        statusBar.insertBefore(divB, statusInfo);
+        document.getElementById('biscuitButton').addEventListener('click', function() {
+            navigator.clipboard.writeText(version);
+        });
     }
+    const divL = document.createElement('divL');
+    divL.classList.add('button-toolbar');
+    divL.id = 'statusInfoToggle';
+    divL.setAttribute('title', 'Toggle status info');
+    divL.innerHTML = '<button draggable="false" tabindex="-1"><svg width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1520 1216q0-40-28-68l-208-208q-28-28-68-28-42 0-72 32 3 3 19 18.5t21.5 21.5 15 19 13 25.5 3.5 27.5q0 40-28 68t-68 28q-15 0-27.5-3.5t-25.5-13-19-15-21.5-21.5-18.5-19q-33 31-33 73 0 40 28 68l206 207q27 27 68 27 40 0 68-26l147-146q28-28 28-67zm-703-705q0-40-28-68l-206-207q-28-28-68-28-39 0-68 27l-147 146q-28 28-28 67 0 40 28 68l208 208q27 27 68 27 42 0 72-31-3-3-19-18.5t-21.5-21.5-15-19-13-25.5-3.5-27.5q0-40 28-68t68-28q15 0 27.5 3.5t25.5 13 19 15 21.5 21.5 18.5 19q33-31 33-73zm895 705q0 120-85 203l-147 146q-83 83-203 83-121 0-204-85l-206-207q-83-83-83-203 0-123 88-209l-88-88q-86 88-208 88-120 0-204-84l-208-208q-84-84-84-204t85-203l147-146q83-83 203-83 121 0 204 85l206 207q83 83 83 203 0 123-88 209l88 88q86-88 208-88 120 0 204 84l208 208q84 84 84 204z"/></svg></button>';
+    statusBar.insertBefore(divL, statusInfo);
+    document.getElementById('statusInfoToggle').addEventListener('click', statusInfoLogic);
 };
 
 function statusDropdown() {
@@ -152,7 +149,7 @@ Element.prototype.appendChild = function () {
                 if (!statusContainer) {
                     statusDropdown();
                 }
-                if (statusContainer) {
+                if (statusContainer && !document.getElementById('statusInfoToggle')) {
                     statusContainer.appendChild(document.querySelector('.toolbar-statusbar'));
                     statusMod();
                 }
