d3b6fe7f326e8e6ae4e2d94e7a97b76cd6b4a137
luetage <luetage@gmail.com>
2020-08-19 07:44:38 +0200

    change name of mod

---
+++ panel-doubleclick-automation.js
@@ -0,0 +1,49 @@
+/*
+Panel doubleclick automation
+https://forum.vivaldi.net/topic/49768/done-emulate-double-click-when-left-click-on-favicon-button-in-window-panel/4
+Switches the tab or opens a bookmark, by simulating a doubleclick on a tab or a bookmark in the windows or bookmark panel.
+*/
+
+(function () {
+
+    function doClick(event) {
+        event.stopPropagation();
+        event.preventDefault();
+        var ev = document.createEvent('MouseEvents');
+        ev.initEvent('dblclick', true, true);
+        this.parentNode.dispatchEvent(ev);
+    }
+
+    function checkParents(el, sel) {
+        while (el.parentNode) {
+            el = el.parentNode;
+            if (el.id === sel[0]) {
+                return win;
+            }
+            else if (el.classList !== undefined && el.classList.contains(sel[1])) {
+                return book; 
+            }
+        }
+        return null;
+    }
+    
+    // Set win to 0 or 1 for either triggering the doubbleclick on favicon, or window title.
+    // Set book to 0 or 1 for either triggering the doubleclick on favicon, or bookmark title.
+    const win = 1;
+    const book = 1;
+
+    var appendChild = Element.prototype.appendChild;
+    Element.prototype.appendChild = function () {
+        if (this.tagName === 'LABEL' && arguments[0].tagName === 'IMG') {
+            setTimeout(function() {
+                var check = checkParents(this, ['window-panel', 'panel-bookmarks']);
+                if (check === 0 || check === 1) {
+                    this.childNodes[check].addEventListener('click', doClick);
+                }
+            }.bind(this, arguments[0]));
+        }
+        return appendChild.apply(this, arguments);
+    }
+
+})();
+
