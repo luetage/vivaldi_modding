caadbc57d22f8470b57500f1b427106afc224205
luetage <luetage@gmail.com>
2020-11-30 09:57:04 +0100

    persistent statusinfo logic

--- attack-on-the-statusbar.js
+++ attack-on-the-statusbar.js
@@ -11,29 +11,20 @@
         if (statusInfoToggle.classList.contains('zeig')) {
             statusInfoToggle.classList.remove('zeig');
             statusInfo.removeAttribute('id');
+            var info = 'off';
         }
         else {
             statusInfoToggle.classList.add('zeig');
             statusInfo.id = 'zeig';
+            var info = 'on';
         }
-    }
-
-    function statusStyle() {
-        const style = document.createElement('style');
-        style.type = 'text/css';
-        style.id = 'statusMod';
-        style.innerHTML = `#statusContainer {position: absolute;z-index: 1;max-width: 100vw;right: 0;top: 0;box-shadow: 0 2px 6px rgba(0, 0, 0, 0.25);}.toolbar-statusbar {display: none;border-top: none;border-bottom: 1px solid var(--colorBorder);}#statusContainer .toolbar-statusbar {display: flex}.toolbar-statusbar .button-popup.button-popup-above {bottom: unset;top: 22px;}.toolbar-statusbar .button-popup.button-popup-above:before, .toolbar-statusbar .button-popup.button-popup-above:after {opacity: 0;}.biscuit-setting-version {display: none !important;}#biscuitButton button svg, #statusInfoToggle button svg {width: 14px;height: 14px;}#statusInfoToggle.zeig button svg {fill: var(--colorHighlightBg);}.StatusInfo {display: none;}#zeig.StatusInfo.StatusInfo--Visible {display: inline-block;}`;
-        document.getElementsByTagName('head')[0].appendChild(style);
+        chrome.storage.local.set ({'statusInfo': info});
     }
 
     function statusMod() {
-        if (!document.getElementById('statusMod')) {
-            statusStyle();
-        }
         const cont = document.createElement('div');
         const statusBar = document.querySelector('.toolbar-statusbar');
         const statusInfo = document.querySelector('.StatusInfo');
-        statusInfo.id = 'zeig';
         cont.id = 'statusContainer';
         document.querySelector('.inner').appendChild(cont);
         cont.appendChild(statusBar);
@@ -52,11 +43,17 @@
         const divL = document.createElement('divL');
         divL.classList.add('button-toolbar');
         divL.id = 'statusInfoToggle';
-        divL.classList.add('zeig');
         divL.setAttribute('title', 'Toggle status info');
         divL.innerHTML = '<button draggable="false" tabindex="-1"><svg width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1520 1216q0-40-28-68l-208-208q-28-28-68-28-42 0-72 32 3 3 19 18.5t21.5 21.5 15 19 13 25.5 3.5 27.5q0 40-28 68t-68 28q-15 0-27.5-3.5t-25.5-13-19-15-21.5-21.5-18.5-19q-33 31-33 73 0 40 28 68l206 207q27 27 68 27 40 0 68-26l147-146q28-28 28-67zm-703-705q0-40-28-68l-206-207q-28-28-68-28-39 0-68 27l-147 146q-28 28-28 67 0 40 28 68l208 208q27 27 68 27 42 0 72-31-3-3-19-18.5t-21.5-21.5-15-19-13-25.5-3.5-27.5q0-40 28-68t68-28q15 0 27.5 3.5t25.5 13 19 15 21.5 21.5 18.5 19q33-31 33-73zm895 705q0 120-85 203l-147 146q-83 83-203 83-121 0-204-85l-206-207q-83-83-83-203 0-123 88-209l-88-88q-86 88-208 88-120 0-204-84l-208-208q-84-84-84-204t85-203l147-146q83-83 203-83 121 0 204 85l206 207q83 83 83 203 0 123-88 209l88 88q86-88 208-88 120 0 204 84l208 208q84 84 84 204z"/></svg></button>';
         statusBar.insertBefore(divL, statusInfo);
         document.getElementById('statusInfoToggle').addEventListener('click', statusInfoLogic);
+        chrome.storage.local.get({'statusInfo': 'on'}, function(check) {
+            const info = check.statusInfo;
+            if (info === 'on') {
+                document.querySelector('.StatusInfo').id = 'zeig';
+                document.getElementById('statusInfoToggle').classList.add('zeig');
+            }
+        })
     }
 
     var appendChild = Element.prototype.appendChild;
