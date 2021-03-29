4798ba33748a671162b5d81a9d4a87e93998d5f2
luetage <luetage@vivaldi.net>
2021-03-29 22:06:05 +0200

    switch to J2000

--- history-moon.js
+++ history-moon.js
@@ -9,9 +9,9 @@
         phases: [['New', 0, 1], ['Waxing Crescent', 1, 6.38264692644], ['First Quarter', 6.38264692644, 8.38264692644], ['Waxing Gibbous', 8.38264692644, 13.76529385288], ['Full', 13.76529385288, 15.76529385288], ['Waning Gibbous', 15.76529385288, 21.14794077932], ['Last Quarter', 21.14794077932, 23.14794077932], ['Waning Crescent', 23.14794077932, 28.53058770576], ['end', 28.53058770576, 29.53058770576]],
         phase: () => {
             const lunarcycle = 29.53058770576;
-            const lunartime = lunarcycle * (24 * 60 * 60);
-            const unixtime = Math.floor(Date.now()/1000);
-            const newmoon = 1610514000;
+            const lunartime = lunarcycle * 86400;
+            const unixtime = Math.round(Date.now()/1000);
+            const newmoon = 947182440;
             const diff = unixtime - newmoon;
             const mod = diff % lunartime;
             const frac = mod / lunartime;
@@ -19,22 +19,20 @@
             for (let i = 0; i < 9; i++) {
                 if (age >= moon.phases[i][1] && age <= moon.phases[i][2]) {
                     if (i === 8) i = 0;
-                    return {phase: i, name: moon.phases[i][0], progress: Math.trunc(100 * frac)};
+                    return {phase: i, name: moon.phases[i][0], progress: Math.trunc(frac * 100)};
                 }
             }
         }
     }
 
-    let historymoon = () => {
+    let historymoon = phase => {
         const hbtn = document.querySelector('#switch button.history span');
-        hbtn.innerHTML = `<svg width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 216.2 216.2" class="history-moon"><path class="history-moon-${_p.phase}" d="" fill-rule="evenodd"></path></svg>`;
+        hbtn.innerHTML = `<svg width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewbox="0 0 216.2 216.2" class="history-moon"><path class="history-moon-${phase}" d="" fill-rule="evenodd"></path></svg>`;
     }
 
-    let repel = mutations => {
+    let moonwatch = (mutations, phase) => {
         mutations.forEach(mutation => {
-            if (mutation.attributeName === 'class') {
-               historymoon(); 
-            }
+            if (mutation.attributeName === 'class') historymoon(phase);
         })
     }
 
@@ -43,11 +41,12 @@
         if (this.tagName === 'BUTTON') {
             setTimeout(function() {
                 if (this.classList.contains('panelbtn') && this.classList.contains('history')) {
-                    _p = moon.phase();
-                    historymoon();
-                    this.title += '\n' + _p.name + ' Moon ' + _p.progress + '%';
-                    const moonwatch = new MutationObserver(repel);
-                    moonwatch.observe(this, {attributes: true});
+                    const p = moon.phase();
+                    historymoon(p.phase);
+                    this.title += '\n' + p.name + ' Moon ' + p.progress + '%';
+                    const mw = (mutations) => moonwatch(mutations, p.phase);
+                    const resist = new MutationObserver(mw);
+                    resist.observe(this, {attributes: true});
                 }
             }.bind(this, arguments[0]));
         }
