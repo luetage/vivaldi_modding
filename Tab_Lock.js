d31b18a3c2e5f65def4fd1e81d8d6cfae7bfaf5b
luetage <luetage@vivaldi.net>
2021-04-13 15:20:32 +0200

    new tab lock page action

--- Tab_Lock.js
+++ Tab_Lock.js
@@ -1,17 +1,6 @@
 // Tab Lock
 // https://forum.vivaldi.net/topic/30957/tab-lock
-// Tries to prevent any kind of navigation.
-
-document.body.addEventListener('click', event => {
-    let target = event.target;
-    do {
-        if (target.nodeName.toUpperCase() === 'A' && target.href) {
-        target.href = 'javascript:'; //disables all links
-        //target.target = '_blank'; //opens links in new tab
-        break;
-        }
-    } while (target = target.parentElement);
-}, true)
+// Throws a warning when you try to navigate.
 
 window.addEventListener('beforeunload', event => {
     event.preventDefault();
