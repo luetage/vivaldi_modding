67320f6ac6fbba590fcd87e634d8810b6413ef9a
luetage <luetage@vivaldi.net>
2021-10-01 07:58:31 +0200

    update formatting

--- load/collapse-keyboard-settings.js
+++ load/collapse-keyboard-settings.js
@@ -1,16 +1,17 @@
 // Collapse Keyboard Settings
 // version 2021.9.0
 // https://forum.vivaldi.net/topic/24543/keyboard-settings-should-default-to-collapsed/6
-// Collapses the keyboard settings items in vivaldi://settings/keyboard.
+// Automatically collapses the keyboard settings items in
+// vivaldi://settings/keyboard.
 
 (function () {
   const settingsUrl =
     "chrome-extension://mpognobbkildjkofajifpdfhcoklimli/components/settings/settings.html?path=";
   chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.url === `${settingsUrl}keyboard`)
-      setTimeout(
-        () => document.querySelector(".category.show button").click(),
-        100
-      );
+    if (changeInfo.url === `${settingsUrl}keyboard`) {
+      setTimeout(() => {
+        document.querySelector(".category.show button").click();
+      }, 100);
+    }
   });
 })();
