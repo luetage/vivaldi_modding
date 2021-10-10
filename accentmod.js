4aa6c8eca2cf8d7451772ef10a9949fcb082e093
luetage <luetage@vivaldi.net>
2021-10-10 15:37:55 +0200

    accent mod remove unused variable

--- accentmod.js
+++ accentmod.js
@@ -9,7 +9,7 @@
     hex
       .replace(
         /^#?([a-f\d])([a-f\d])([a-f\d])$/i,
-        (m, r, g, b) => "#" + r + r + g + g + b + b
+        (r, g, b) => "#" + r + r + g + g + b + b
       )
       .substring(1)
       .match(/.{2}/g)
