aacc159e72cea49b430c0c34a16e8d982b954489
luetage <luetage@vivaldi.net>
2021-11-16 13:36:15 +0100

    implement css toggle

--- load/theme-interface-plus.js
+++ load/theme-interface-plus.js
@@ -1,27 +1,30 @@
 // Theme Interface plus
-// version 2021.11.3
+// version 2021.11.4
 // https://forum.vivaldi.net/topic/68564/theme-interface-plus
 // Adds functionality to toggle system themes, sort user themes alphabetically
 // and move themes individually to Vivaldi’s settings page.
 
 (function () {
-  let toggle = (action) => {
-    vivaldi.prefs.get("vivaldi.themes.system", (system) => {
-      const check = document.querySelector(".ThemePreview:first-of-type");
-      if (action === "hide") {
-        disp = "none";
-      } else if (check.style.display === "none") {
-        disp = "inline-flex";
-        systemDefault = 1;
-      } else {
-        disp = "none";
-        systemDefault = 0;
+  let toggle = (init) => {
+    const css = document.getElementById("tipCSS");
+    if (
+      (systemDefault === 0 && init === 1) ||
+      (systemDefault === 1 && init !== 1)
+    ) {
+      if (!css) {
+        vivaldi.prefs.get("vivaldi.themes.system", (sys) => {
+          const hide = document.createElement("style");
+          hide.setAttribute("type", "text/css");
+          hide.id = "tipCSS";
+          hide.innerText = `.ThemePreviews .ThemePreview:nth-child(-n+${sys.length}){display: none}`;
+          document.getElementsByTagName("head")[0].appendChild(hide);
+        });
       }
-      const themes = document.querySelectorAll(".ThemePreview");
-      for (let i = 0; i < system.length; i++) {
-        themes[i].style.display = disp;
-      }
-    });
+      systemDefault = 0;
+    } else {
+      if (css) css.parentNode.removeChild(css);
+      systemDefault = 1;
+    }
   };
 
   let sort = () => {
@@ -63,12 +66,11 @@
       ["◂", "Move Theme Left", moveL],
       ["▸", "Move Theme Right", moveR],
     ],
-    init: () => {
-      if (systemDefault === 0) toggle("hide");
+    load: () => {
       const footer = document.querySelector(".TabbedView-Footer");
       const link = document.querySelector(".TabbedView-Footer a");
-      if (!footer.classList.contains("tipbtn")) {
-        footer.classList.add("tipbtn");
+      if (!footer.classList.contains("tipBtn")) {
+        footer.classList.add("tipBtn");
         goUI.buttons.forEach((button) => {
           let b = document.createElement("div");
           b.classList.add("button-toolbar");
@@ -83,13 +85,14 @@
   let systemDefault = 0; // set to »1« to display system themes by default
   const settingsUrl =
     "chrome-extension://mpognobbkildjkofajifpdfhcoklimli/components/settings/settings.html?path=";
+  toggle(1);
   chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     if (changeInfo.url === `${settingsUrl}themes`) {
-      setTimeout(goUI.init, 100);
+      setTimeout(goUI.load, 100);
       document
         .querySelector(".TabbedView-List button:first-of-type")
         .addEventListener("click", () => {
-          setTimeout(goUI.init, 100);
+          setTimeout(goUI.load, 100);
         });
     }
   });
