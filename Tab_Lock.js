e8e8520e1051f9d72953eb46a2f9be51d29f896b
luetage <luetage@vivaldi.net>
2022-02-27 12:58:39 +0100

    change explanation for tab lock

--- Tab_Lock.js
+++ Tab_Lock.js
@@ -1,13 +1,13 @@
 // Tab Lock
 // version 2021.9.0
 // https://forum.vivaldi.net/topic/30957/tab-lock/24
-// Custom page action. Throws a warning when you try to navigate.
+// Custom page action. Throws a warning when you try to navigate. Please read
+// installation instructions on the forum.
 
 (function () {
   window.addEventListener("beforeunload", (event) => {
     event.preventDefault();
     event.returnValue = "";
   });
-
   window.addEventListener("popstate", () => console.log("block navigation"));
 })();
