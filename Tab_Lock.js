97d0ad218251a48395bdfce1d35b5b84e7c14660
luetage <luetage@vivaldi.net>
2022-03-19 23:46:07 +0100

    update forum link

--- Tab_Lock.js
+++ Tab_Lock.js
@@ -1,6 +1,6 @@
 // Tab Lock
 // version 2021.9.0
-// https://forum.vivaldi.net/topic/30957/tab-lock/24
+// https://forum.vivaldi.net/post/241508
 // Custom page action. Throws a warning when you try to navigate. Please read
 // installation instructions on the forum.
 
