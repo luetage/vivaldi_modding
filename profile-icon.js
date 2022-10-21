ce011674c30f81a738ea7672611e2336caa9a29f
luetage <luetage@vivaldi.net>
2022-10-21 11:17:45 +0200

    simplify profile icon

--- profile-icon.js
+++ profile-icon.js
@@ -1,5 +1,5 @@
 // Profile Icon
-// version 2022.4.0
+// version 2022.10.0
 // https://forum.vivaldi.net/post/522106
 // Exchanges your account/default profile image (sync), for a proper SVG icon
 // using theme colors. Credits to tam710562 for coming up with a solution to
@@ -7,21 +7,14 @@
 // Free.
 
 (function profileIcon() {
-  function replace(icon) {
-    icon.innerHTML =
-      '<svg width="18" height="18" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM256 128c39.77 0 72 32.24 72 72S295.8 272 256 272c-39.76 0-72-32.24-72-72S216.2 128 256 128zM256 448c-52.93 0-100.9-21.53-135.7-56.29C136.5 349.9 176.5 320 224 320h64c47.54 0 87.54 29.88 103.7 71.71C356.9 426.5 308.9 448 256 448z"></path></svg>';
-    const svgIcon = document.querySelector(".profile-popup button svg");
-    svgIcon.style.height = "18px";
-    svgIcon.style.width = "18px";
-  }
-
   let appendChild = Element.prototype.appendChild;
   Element.prototype.appendChild = function () {
     if (arguments[0].tagName === "BUTTON") {
       setTimeout(
         function () {
           if (this.classList.contains("profile-popup")) {
-            replace(arguments[0]);
+            arguments[0].innerHTML =
+              '<svg width="18" height="18" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" style="height:18px;width:18px"><path d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM256 128c39.77 0 72 32.24 72 72S295.8 272 256 272c-39.76 0-72-32.24-72-72S216.2 128 256 128zM256 448c-52.93 0-100.9-21.53-135.7-56.29C136.5 349.9 176.5 320 224 320h64c47.54 0 87.54 29.88 103.7 71.71C356.9 426.5 308.9 448 256 448z"></path></svg>';
           }
         }.bind(this, arguments[0])
       );
