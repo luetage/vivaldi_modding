8eeb3c1d655c8f31dabbdeb8bb493469922098fa
luetage <luetage@vivaldi.net>
2023-02-05 08:06:59 +0100

    fix profile icon mod

--- profile-icon.js
+++ profile-icon.js
@@ -1,5 +1,5 @@
 // Profile Icon
-// version 2022.10.0
+// version 2023.2.0
 // https://forum.vivaldi.net/post/522106
 // Exchanges your account/default profile image (sync), for a proper SVG icon
 // using theme colors. Credits to tam710562 for coming up with a solution to
@@ -12,9 +12,10 @@
     if (arguments[0].tagName === "BUTTON") {
       setTimeout(
         function () {
-          if (this.classList.contains("profile-popup")) {
-            arguments[0].innerHTML =
-              '<svg width="18" height="18" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" style="height:18px;width:18px"><path d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM256 128c39.77 0 72 32.24 72 72S295.8 272 256 272c-39.76 0-72-32.24-72-72S216.2 128 256 128zM256 448c-52.93 0-100.9-21.53-135.7-56.29C136.5 349.9 176.5 320 224 320h64c47.54 0 87.54 29.88 103.7 71.71C356.9 426.5 308.9 448 256 448z"></path></svg>';
+          if (this.classList.contains("ToolbarButton-Button")) {
+            if (this.name === "AccountButton") {
+              this.innerHTML = `<span><svg width="28" height="28" viewBox="0 0 28 28" xmlns="http://www.w3.org/2000/svg"><path d="M 19.090434,18.563592 C 18.30373,17.19665 16.826437,16.278236 15.139118,16.278236 h -2.278236 c -1.687318,0 -3.1646121,0.918414 -3.9513154,2.285356 1.2530304,1.395419 3.0684994,2.271116 5.0904334,2.271116 2.021935,0 3.837404,-0.879256 5.090434,-2.271116 z M 23.112944,14 c 0,5.033478 -4.079466,9.112944 -9.112944,9.112944 -5.0334775,0 -9.1129439,-4.079466 -9.1129439,-9.112944 0,-5.0334775 4.0794664,-9.1129439 9.1129439,-9.1129439 5.033478,0 9.112944,4.0794664 9.112944,9.1129439 z M 14,14.569559 c 1.416778,0 2.563016,-1.146237 2.563016,-2.563015 0,-1.416778 -1.146238,-2.5630158 -2.563016,-2.5630158 -1.416778,0 -2.563015,1.1462378 -2.563015,2.5630158 0,1.416778 1.146237,2.563015 2.563015,2.563015 z"></path></svg></span>`;
+            }
           }
         }.bind(this, arguments[0])
       );
