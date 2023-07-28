3197abef2d7b8dd7ab40c014548261d84775450e
luetage <luetage@vivaldi.net>
2023-07-28 11:45:31 +0200

    update

--- load/toolbar-icons-mod.js
+++ load/toolbar-icons-mod.js
@@ -4,7 +4,6 @@
 
 (function toolbarIconsMod() {
   function toggleKeysAndGestures(e, el) {
-    console.log('hi');
     vivaldi.prefs.get(sk, (k) => {
       vivaldi.prefs.get(sm, (m) => {
         if (e !== null && !e.ctrlKey) {
