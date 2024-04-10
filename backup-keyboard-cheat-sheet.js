d24e38a3113458c6811674e737808e3292e17bc3
luetage <luetage@vivaldi.net>
2024-04-10 14:49:58 +0200

    new: cheatsheet mod

---
+++ backup-keyboard-cheat-sheet.js
@@ -0,0 +1,24 @@
+// Backup Keyboard Cheat Sheet
+// version 2024.4.0
+// https://
+// Writes the content of the keyboard cheat sheet to your clipboard in markdown
+// format. Open keyboard cheat sheet popup, open user interface console, paste
+// code and hit Enter to execute.
+
+let output = "";
+const wrapper = document.querySelector(".keyboardShortcutsWrapper");
+const title = wrapper.querySelector("h1");
+output += `# ${title.innerText}\n\n| &nbsp; | &nbsp; |\n|---|---|\n`;
+wrapper.querySelectorAll(".category").forEach((category) => {
+  output += `| **${category.firstChild.innerText.toUpperCase()}** |   |\n`;
+  category.querySelectorAll(".keycombo").forEach((key) => {
+    output += `| ${key.innerText} |`;
+    key.querySelectorAll("input").forEach((combo) => {
+      output += ` ${combo.value}<br>`;
+    });
+    output += ` |\n`;
+  });
+  output += `| &nbsp; | &nbsp; |\n`;
+});
+copy(output);
+console.info(output);
