8c81ffca8296b8346a5098e2b7a1436f86a0c5a4
luetage <luetage@vivaldi.net>
2024-04-11 16:24:52 +0200

    update

--- backup-keyboard-cheat-sheet.js
+++ backup-keyboard-cheat-sheet.js
@@ -1,7 +1,7 @@
 // Backup Keyboard Cheat Sheet
 // version 2024.4.1
 // https://forum.vivaldi.net/post/745999
-// Writes the content of the keyboard cheat sheet to your clipboard in markdown
+// Writes the contents of the keyboard cheat sheet to your clipboard in markdown
 // format. Open keyboard cheat sheet popup, open user interface console, paste
 // code and hit Enter to execute.
 
