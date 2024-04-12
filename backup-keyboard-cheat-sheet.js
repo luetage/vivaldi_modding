a6734da8e25b23ac44faf9064d323434e7706851
luetage <luetage@vivaldi.net>
2024-04-12 06:59:52 +0200

    fix page breaks for cheat sheet

--- backup-keyboard-cheat-sheet.js
+++ backup-keyboard-cheat-sheet.js
@@ -1,5 +1,5 @@
 // Backup Keyboard Cheat Sheet
-// version 2024.4.1
+// version 2024.4.2
 // https://forum.vivaldi.net/post/745999
 // Writes the contents of the keyboard cheat sheet to your clipboard in markdown
 // format. Open keyboard cheat sheet popup, open user interface console, paste
@@ -9,7 +9,8 @@ const sheet = document.querySelector(".keyboardShortcutsWrapper");
 const heading = sheet.querySelector("h1");
 let output = `# ${heading.innerText}\n\n| &nbsp; | &nbsp; |\n|---|---|\n`;
 sheet.querySelectorAll(".category").forEach((category) => {
-  output += `| **${category.firstChild.innerText.toUpperCase()}** |   |\n`;
+  const caps = category.firstChild.innerText.toUpperCase();
+  output += `| **${caps}** | <div style="page-break-after:avoid"></div> |\n`;
   category.querySelectorAll(".keycombo").forEach((key) => {
     output += `| ${key.innerText} |`;
     key.querySelectorAll("input").forEach((combo) => {
