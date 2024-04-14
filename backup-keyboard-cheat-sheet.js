dafc9c66d10084cd5546a15332ea791ddda5cd70
luetage <luetage@vivaldi.net>
2024-04-14 08:09:06 +0200

    improve keyboard cheat sheet

--- backup-keyboard-cheat-sheet.js
+++ backup-keyboard-cheat-sheet.js
@@ -1,5 +1,5 @@
 // Backup Keyboard Cheat Sheet
-// version 2024.4.3
+// version 2024.4.4
 // https://forum.vivaldi.net/post/745999
 // Writes the contents of the keyboard cheat sheet to your clipboard in markdown
 // format. Open keyboard cheat sheet popup, open user interface console, paste
@@ -7,19 +7,22 @@
 
 const sheet = document.querySelector(".keyboardShortcutsWrapper");
 const heading = sheet.querySelector("h1");
-const pb = "<div style='page-break-after:avoid'></div>";
+const pb = `<div style="page-break-after:avoid"></div>`;
 let output = `# ${heading.innerText}\n\n<table>\n`;
-sheet.querySelectorAll(".category").forEach((category) => {
+sheet.querySelectorAll(".category").forEach((category, key, arr) => {
   const caps = category.firstChild.innerText.toUpperCase();
-  output += `<tr><td><b>${caps}</b></td><td>${pb}</td></tr>\n`;
+  output += `  <tr><td><b>${caps}</b></td><td>${pb}</td></tr>\n`;
   category.querySelectorAll(".keycombo").forEach((command) => {
-    output += `<tr><td>${command.innerText}</td><td>`;
-    command.querySelectorAll("input").forEach((combo) => {
-      output += `${combo.value}<br>`;
+    output += `  <tr><td>${command.innerText}</td><td>`;
+    command.querySelectorAll("input").forEach((combo, key, arr) => {
+      output += combo.value;
+      if (!Object.is(arr.length - 1, key)) output += "<br>";
     });
     output += "</td></tr>\n";
   });
-  output += "<tr><td>&nbsp;</td><td>&nbsp;</td></tr>\n";
+  if (!Object.is(arr.length - 1, key)) {
+    output += "  <tr><td>&nbsp;</td><td>&nbsp;</td></tr>\n";
+  }
 });
 output += "</table>";
 copy(output);
