e8d2ac1147e0b7a9d3c0560f76ebffdc7810db63
luetage <luetage@vivaldi.net>
2024-04-15 07:05:00 +0200

    update

--- backup-keyboard-cheat-sheet.js
+++ backup-keyboard-cheat-sheet.js
@@ -1,5 +1,5 @@
 // Backup Keyboard Cheat Sheet
-// version 2024.4.4
+// version 2024.4.5
 // https://forum.vivaldi.net/post/745999
 // Writes the contents of the keyboard cheat sheet to your clipboard in markdown
 // format. Open keyboard cheat sheet popup, open user interface console, paste
@@ -7,7 +7,7 @@
 
 const sheet = document.querySelector(".keyboardShortcutsWrapper");
 const heading = sheet.querySelector("h1").innerText;
-const pb1 = `<style>\n  tr{page-break-inside:avoid}\n</style>\n\n`;
+const pb1 = "<style>\n  tr{page-break-inside:avoid}\n</style>\n\n";
 const pb2 = `<div style="page-break-after:avoid"></div>`;
 let output = `${pb1}# ${heading}\n\n<table>\n`;
 sheet.querySelectorAll(".category").forEach((category, key, arr) => {
@@ -17,14 +17,12 @@ sheet.querySelectorAll(".category").forEach((category, key, arr) => {
     output += `  <tr><td>${command.innerText}</td><td>`;
     command.querySelectorAll("input").forEach((combo, key, arr) => {
       output += combo.value;
-      if (!Object.is(arr.length - 1, key)) output += "<br>";
+      if (Object.is(arr.length - 1, key)) output += "</td></tr>\n";
+      else output += "<br>";
     });
-    output += "</td></tr>\n";
   });
-  if (!Object.is(arr.length - 1, key)) {
-    output += "  <tr><td>&nbsp;</td><td>&nbsp;</td></tr>\n";
-  }
+  if (Object.is(arr.length - 1, key)) output += "</table>";
+  else output += "  <tr><td>&nbsp;</td><td>&nbsp;</td></tr>\n";
 });
-output += "</table>";
 copy(output);
 console.info(output);
