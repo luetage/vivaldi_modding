a607adfd68bff044d46d6a523560c9a553d70fc9
luetage <luetage@vivaldi.net>
2024-09-12 08:18:52 +0200

    delete sync.js page action, Vivaldi syncs fast now

--- page-actions/Sync.js
+++
@@ -1,9 +0,0 @@
-// Sync
-// version 2023.1.0
-// Custom page action for use in a command chain. Triggers the update button on
-// the internal sync page.
-
-(function () {
-  document.getElementById("trigger-refresh").click();
-})();
-
