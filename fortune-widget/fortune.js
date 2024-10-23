b1c4911c25fbe6b351c0989be7041af629521985
luetage <luetage@vivaldi.net>
2024-10-23 15:58:34 +0200

    local fortune widget

--- fortune-widget/fortune.js
+++ fortune-widget/fortune.js
@@ -5,84 +5,23 @@
 
 "use strict";
 
-async function get_data() {
-  return new Promise((resolve, reject) => {
-    fetch("https://helloacm.com/api/fortune/")
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then((data) => {
-        resolve({
-          success: true,
-          data: data,
-          message: "data loaded",
-        });
-      })
-      .catch((error) => {
-        reject({
-          success: false,
-          error: error.message,
-          message: "cannot load data :/",
-        });
-      });
+function write_fortune(el) {
+  const rnd = data[Math.floor(Math.random() * data.length)];
+  rnd.quote = rnd.quote.replace(/--/g, "\u2013");
+  rnd.quote = rnd.quote.replace(/\n\n/g, "<br><br>");
+  const fortune = rnd.author
+    ? `${rnd.quote}<br>\u2015\u202f${rnd.author}`
+    : rnd.quote;
+  el.innerHTML = fortune;
+}
+
+function init() {
+  const text = document.getElementById("text");
+  const asterism = document.getElementById("asterism");
+  write_fortune(text);
+  asterism.addEventListener("click", (e) => {
+    if (e.button === 0 && e.ctrlKey) write_fortune(text);
   });
 }
 
-function get_time() {
-  const date = new Date();
-  const d = date.getDay();
-  const h = date.getHours();
-  return { day: d, hour: h };
-}
-
-function write_fortune(q, el) {
-  q = q.replace("--", "-\u2060-");
-  el.innerHTML = q;
-}
-
-const init = async () => {
-  let request = true;
-  const text = document.getElementById("text");
-  const sto = JSON.parse(localStorage.getItem("fortune"));
-  if (sto !== null) {
-    console.info(sto);
-    const local = get_time();
-    if (
-      local.day === sto.time.day &&
-      (local.hour < 12 || (local.hour > 11 && sto.time.hour > 11))
-    ) {
-      request = false;
-      write_fortune(sto.quote, text);
-    }
-  }
-  if (request === true) {
-    await get_data().then(
-      (resolve) => {
-        console.info(resolve);
-        const store = get_time();
-        const object = {
-          quote: resolve.data,
-          time: { day: store.day, hour: store.hour },
-        };
-        localStorage.setItem("fortune", JSON.stringify(object));
-        write_fortune(resolve.data, text);
-      },
-      (reject) => {
-        text.innerHTML = reject.message;
-      }
-    );
-  }
-};
-
-function new_fortune(event) {
-  if (event.button === 0 && event.ctrlKey) {
-    localStorage.removeItem("fortune");
-    init();
-  }
-}
-
 init();
-document.getElementById("asterism").addEventListener("click", new_fortune);
