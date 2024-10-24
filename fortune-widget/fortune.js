bae141a2874d5a97ec52405de07ac7cba4f91ecf
luetage <luetage@vivaldi.net>
2024-10-24 18:56:02 +0200

    also fix author typography

--- fortune-widget/fortune.js
+++ fortune-widget/fortune.js
@@ -5,22 +5,26 @@
 
 "use strict";
 
+function typ(x) {
+  const bold = /(?<!\*)\*\*(\w)([^*]*?\w)?\*\*(?!\*)/g;
+  const italic = /(?<!\*)\*(\w)([^*]*?\w)?\*(?!\*)/g;
+  x = x.replace(bold, "<b>$1$2</b>");
+  x = x.replace(italic, "<i>$1$2</i>");
+  x = x.replace(/---/g, "\u2014");
+  x = x.replace(/--/g, "\u2013");
+  x = x.replace(/(\d)-(\d)/g, "$1\u2013$2");
+  x = x.replace(/(\w)-(\w)/g, "$1\u2010$2");
+  x = x.replace(/ - /g, " \u2013 ");
+  x = x.replace(/\n\n/g, "<br>");
+  return x;
+}
+
 function write_fortune(el) {
   const random = Math.floor(Math.random() * data.length);
   const rnd = data[random];
-  const bold = /(?<!\*)\*\*(\w)([^*]*?\w)?\*\*(?!\*)/g;
-  const italic = /(?<!\*)\*(\w)([^*]*?\w)?\*(?!\*)/g;
-  rnd.quote = rnd.quote.replace(bold, "<b>$1$2</b>");
-  rnd.quote = rnd.quote.replace(italic, "<i>$1$2</i>");
-  rnd.quote = rnd.quote.replace(/---/g, "\u2014");
-  rnd.quote = rnd.quote.replace(/--/g, "\u2013");
-  rnd.quote = rnd.quote.replace(/(\d)-(\d)/g, "$1\u2013$2");
-  rnd.quote = rnd.quote.replace(/(\w)-(\w)/g, "$1\u2010$2");
-  rnd.quote = rnd.quote.replace(/ - /g, " \u2013 ");
-  rnd.quote = rnd.quote.replace(/\n\n/g, "<br>");
   const fortune = rnd.author
-    ? `${rnd.quote}<br>\u2015\u202f${rnd.author}`
-    : rnd.quote;
+    ? `${typ(rnd.quote)}<br>\u2015\u202f${typ(rnd.author)}`
+    : typ(rnd.quote);
   console.info(fortune);
   document.title = `Fortune ${random + 1}`;
   el.innerHTML = fortune;
