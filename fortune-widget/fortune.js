cd7656cf362a3417abf2ebd01fc7d661d2dbff8b
luetage <luetage@vivaldi.net>
2024-10-24 16:15:38 +0200

    update

--- fortune-widget/fortune.js
+++ fortune-widget/fortune.js
@@ -1,6 +1,6 @@
 // Fortune Widget
 // version 2024.10.0
-// Guide and updates ☛ https://forum.vivaldi.net/post/783627
+// Guide and updates ☛ https://forum.vivaldi.net/post/787840
 // ————————  ⁂  ————————
 
 "use strict";
