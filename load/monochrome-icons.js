6a68450fa176039d5faf3079055675a9a61c676b
luetage <luetage@vivaldi.net>
2026-01-23 09:13:05 +0100

    move all color calculations to oklch

--- load/monochrome-icons.js
+++ load/monochrome-icons.js
@@ -1,36 +1,26 @@
 // Monochrome icons
-// version 2024.11.1
+// version 2026.1.0
 // https://forum.vivaldi.net/post/791344
 // Makes web panel thumbnails monochrome depending on theme colors.
 
 (async function monochrome_icons() {
   "use strict";
 
-  function convert(srgb) {
-    const val = srgb.slice(11, -1).trim().split(/\s+/);
-    const r = Math.round(parseFloat(val[0]) * 255);
-    const g = Math.round(parseFloat(val[1]) * 255);
-    const b = Math.round(parseFloat(val[2]) * 255);
-    const calc =
-      (Math.atan2(Math.sqrt(3) * (g - b), 2 * r - g - b) * 180) / Math.PI;
-    return (calc - 38.71).toFixed(2);
-  }
-
   function theme(css) {
     const color = document.getElementById("main");
     color.style =
-      "color: color-mix(in hsl, var(--colorFgFadedMost) 70%, var(--colorHighlightBg));";
-    const srgb = getComputedStyle(color).getPropertyValue("color");
+      "color: color-mix(in oklch, var(--colorBg) 80%, var(--colorHighlightBg));";
+    const oklch = getComputedStyle(color).getPropertyValue("color");
     color.removeAttribute("style");
-    const hue = convert(srgb);
-    console.info(`hue-change: ${hue}°`);
+    const hue = (Number(oklch.match(/-?\d+(\.\d+)?/g)[2]) - 50).toFixed(2);
+    console.info(`monochrome-icons hue-change: ${hue}°`);
     css.innerHTML = `
-.button-toolbar-webpanel img {
-  filter: brightness(0.77) sepia(1) hue-rotate(${hue}deg);
-}
-#browser.isblurred.dim-blurred .button-toolbar-webpanel img {
-  filter: brightness(0.77) sepia(1) hue-rotate(${hue}deg) opacity(0.65) !important;
-}
+      .button-toolbar-webpanel img {
+        filter: brightness(0.77) sepia(1) hue-rotate(${hue}deg);
+      }
+      #browser.isblurred.dim-blurred .button-toolbar-webpanel img {
+        filter: brightness(0.77) sepia(1) hue-rotate(${hue}deg) opacity(0.65) !important;
+      }
     `;
   }
 
